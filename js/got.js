61c77f65753741b28a86a1b979c08f3db54db5a2
GMiki <gmiki91@gmail.com>
2018-12-06 17:36:23 +0100

    filtered name list shown

--- js/got.js
+++ js/got.js
@@ -13,7 +13,9 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
-  lol(userDatas);
+  var aliveData = removeDeadPeople(userDatas);
+  var sortedData = sortPeople(aliveData);
+  displayNames(sortedData);
 }
 
 getGameOfThronesCharacterDatas(
@@ -23,7 +25,43 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
-function lol(sajt) {
+function removeDeadPeople(data) {
+  var adat = [];
+  console.log(data[0]);
+  for (var i = 0; i < data.length; i++) {
+    if (!data[i].dead) {
+      adat.push(data[i]);
+    }
+  }
+  return adat;
+}
+
+function sortPeople(data) {
+  var sorban = data.sort( function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  return sorban;
+}
+
+function displayNames(data) {
   var tbody = document.querySelector('tbody');
-  tbody.innerHTML = sajt;
+  for (var i = 0; i < data.length; i++) {
+    if (i % 8 === 0) {
+      var tr = document.createElement('tr');
+    }
+    createTd(data, i, tr);
+    addRowToBody(i, tr, tbody);
+  }
+}
+
+function createTd(data, i, tr) {
+  var td = document.createElement('td');
+  td.innerHTML = data[i].name;
+  tr.appendChild(td);
+}
+
+function addRowToBody(i, tr, tbody) {
+  if (i % 7 === 0) {
+    tbody.appendChild(tr);
+  }
 }
